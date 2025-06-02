894dc12b8f5cc70ec963fc4291f7dfebcbc5f6de
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-01 23:17:53 -0300

    05

---
+++ new react/src/components/Square.tsx
@@ -0,0 +1,16 @@
+
+import Square from '@/components/Square';
+
+
+const Square = () => {
+  return (
+    <div className="w-52 h-52 bg-red-500 text-white flex items-center justify-center">
+      Texto
+    </div>
+
+    <sqaure></sqaure>
+  );
+};
+
+
+export default Page;
