d1a5ea39f79344c870c1ae2c1fc8176b4796d139
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-04 17:17:16 -0300

    h and w

--- react-projeto/src/components/Person.tsx
+++ react-projeto/src/components/Person.tsx
@@ -5,6 +5,8 @@ export const Person = () => {
       <img 
         src="https://i.pinimg.com/736x/e2/ae/91/e2ae91720c347f89d2315edd9ba98ea6.jpg"
         alt="PEdro" 
+        className="w-100 h100"
+        
       />
     </> //serve como um elemento, mas não e um elemento "uma tag transparente "  fragment  //sempre FEHCAR TODAS AS TAGS
   );
