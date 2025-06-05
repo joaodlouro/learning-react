130a296be9cc49b11bc183ea930fe26b5b53c824
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-05 18:26:24 -0300

    props  componentes

--- react-projeto/src/components/Person.tsx
+++ react-projeto/src/components/Person.tsx
@@ -1,9 +1,4 @@
 //serve como um elemento, mas não e um elemento "uma tag transparente "  fragment  //sempre FEHCAR TODAS AS TAGS
-
-const getWeekday = (today: Date) => {
-  return new Intl.DateTimeFormat('en', { weekday: 'long' }).format(today); // dia da semana 
-};
-
 type Props = {
   name: string;
   avatar: string;
@@ -11,22 +6,20 @@ type Props = {
   address?: string;
 };
 
-export const Person = (props: Props) => {
-  const today: Date = new Date();
-
+export const Person = ({ name, avatar, role }: Props) => {
   return (
-    <> 
-      <h1> {props.name}  - {getWeekday(today)} </h1> 
+    <div className="p-3"> 
+      <h1> {name} </h1> 
       <img 
-        src={props.avatar}
-        alt={props.name}
+        src={avatar}
+        alt={name}
         className="w-100 h-100"
       />
       
       <ul>
-        <li>{props.role[0]}</li>
-        <li>{props.role[1]}</li>
+        <li>{role[0]}</li>
+        <li>{role[1]}</li>
       </ul>
-    </>
+    </div>
   );
 };
