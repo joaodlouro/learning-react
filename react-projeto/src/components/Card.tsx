bc5e908be6506b2e5247771c0abe7b8aa23d52d7
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-08 18:23:16 -0300

    &&

--- react-projeto/src/components/Card.tsx
+++ react-projeto/src/components/Card.tsx
@@ -10,9 +10,11 @@ export const Card = ({ phrase, author }: Props) => {
     return (
       <div className="w-96 border-2 border-red p-3 text-3xl text-center italic">
         <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
-        {author && "tem autor"}
+        
+        {author && 
 
-        <p className="text-right text-sm">-{author ?? 'autor'}</p>
+        <p className="text-right text-sm">-{author}</p>
+}
       </div>
     );
   }
