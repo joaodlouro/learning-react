df9465ea4fa5036c4e392306758ec895ca787a67
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-10 15:15:07 -0300

    com <li>

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -2,10 +2,22 @@
 import {peopleList} from '@/data/peopleList';
 
 const Page =  () => {
+
+//const list = peopleList.map(person => <li>{person.name}</li>)  //parametro, list com varios <li>
+
+
 return (
   <div>
     <h1 className="font-bold text-2xl">Olá mundo </h1>
     <h3>algo texto</h3>
+
+    {peopleList.length >0 &&
+    <ul>
+       {peopleList.map (person => 
+      <li key= {person.id}>{person.name}- {person.profession}</li> //quando usar map, user  key= {person.id} (sempre o id), identificador unico para x iten
+      )}
+    </ul>
+}
   </div>
 );
 }
--- react-projeto/src/data/peopleList.ts
+++ react-projeto/src/data/peopleList.ts
@@ -8,6 +8,7 @@ export const peopleList: Person[] = [
       {id: 3, name: 'Fulano3', profession: ',mathematician3'},
         {id: 4, name: 'Fulano4', profession: ',mathematician4'},
           {id: 5, name: 'Fulano5', profession: ',mathematician5'},
+          {id: 6, name: 'Fulano6', profession: ',mathematician6'}
   
 
 ];
