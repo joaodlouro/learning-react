e463416e06334e6a4009e76a737a77eab8340834
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-11 18:21:01 -0300

    {hour >= 0 && hour < 12 && "bom dia"}

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -10,7 +10,9 @@ const Page = () => {
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-blue-500 to-green-500">
       <div className="text-9xl">{fullTime}</div>
       <div className="text-5xl font-bold">
-        {hour < 12 ? "Good morning!" : hour < 18 ? "Good afternoon!" : "Good evening!"}
+           {hour >= 0 && hour < 12 && "bom dia"}
+             {hour >= 12 && hour < 18 && "bom tarde"}
+             {hour >= 18 && hour <= 23 && "bom noite"}
       </div>
     </div>
   );
