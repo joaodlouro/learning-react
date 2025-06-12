354195c5e7c47badb102fe83bce5afdea4b07636
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-12 20:09:48 -0300

    emoji treino

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,21 +1,13 @@
+import { EmojiRating } from "@/components/EmojiRating";
+
 const Page = () => {
-  const fullTime = new Intl.DateTimeFormat("en", {
-    timeStyle: "short",
-    hour12: false,
-  }).format(new Date());
-
-  const hour = new Date().getHours(); 
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-blue-500 to-green-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">
-           {hour >= 0 && hour < 12 && "bom dia"}
-             {hour >= 12 && hour < 18 && "bom tarde"}
-             {hour >= 18 && hour <= 23 && "bom noite"}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      
+       <EmojiRating rate={3.2} />
     </div>
   );
+
 };
 
 export default Page;
---
+++ react-projeto/src/components/EmojiRating.tsx
@@ -0,0 +1,11 @@
+type Props = {            // TIPANDO
+  rate: number;
+}
+
+export const EmojiRating = ({ rate }: Props) => {
+  return (
+    <div>
+     
+    </div>
+  );
+}
