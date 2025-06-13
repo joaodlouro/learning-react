57d0edc2e3a4f7afd3e806ec77a37ecf78a5f2fe
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-13 15:06:54 -0300

    students

---
+++ react-projeto/src/data/students.ts
@@ -0,0 +1,72 @@
+const students= [
+  {
+    id:1,
+    active: true,
+    name: 'Fulano 1',
+    email: 'falono-1@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/0f/f1/ec/0ff1ec3a68937ec4f58a55719a078ae7.jpg',
+    grade1: 7.3,
+    grade2: 8.1
+
+  },
+
+  {
+    id:2,
+    active: true,
+    name: 'Fulano 2',
+    email: 'falono-2@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/bd/db/07/bddb072e2dd134be0cda247afa948e86.jpg',
+    grade1: 2.9,
+    grade2: 8.7
+
+  },
+
+  {
+    id:3,
+    active: true,
+    name: 'Fulano 3',
+    email: 'falono-3@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/bd/f9/96/bdf9961532948379492079389582ba85.jpg',
+    grade1: 8.3,
+    grade2: 6.4
+
+  },
+
+
+    {
+    id:4,
+    active: true,
+    name: 'Fulano 4',
+    email: 'falono-4@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/bd/f9/96/bdf9961532948379492079389582ba85.jpg',
+    grade1: 9,
+    grade2: 8.1
+
+  },
+
+      {
+    id:5,
+    active: true,
+    name: 'Fulano 5',
+    email: 'falono-5@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/68/5c/1f/685c1ff453807d6608bed0258b2cff6b.jpg',
+    grade1: 10,
+    grade2: 9.7
+
+  },
+
+        {
+    id:6,
+    active: true,
+    name: 'Fulano 6',
+    email: 'falono-6@gmail.com',
+    avatar: 'https://i.pinimg.com/736x/2c/85/9b/2c859bc3cb8651c374a10094cc65e84b.jpg',
+    grade1: 5.5,
+    grade2: 6.2
+
+  },
+
+
+
+  
+]
