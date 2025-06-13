1284253c8b7e64499bf7317bc02673f0f29ba85f
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-13 17:25:57 -0300

    /Student

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,13 +1,9 @@
-import { EmojiRating } from "@/components/EmojiRating";
-
 const Page = () => {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      
-       <EmojiRating rate={3.2} />
+  return(
+    <div className="">
+  
     </div>
   );
-
-};
+}
 
 export default Page;
---
+++ react-projeto/src/types/Student.ts
@@ -0,0 +1,11 @@
+export type Students ={
+
+   id: number;
+   active: boolean;
+   name: string;
+   email: string;
+   avatar: string;
+   grade1: number;
+   grade2: number;
+
+}
