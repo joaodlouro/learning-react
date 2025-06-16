1f7d226e1a8fe50c4eb11eab59de996aebabfb8d
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-16 16:26:41 -0300

    lista!!  import { Students } from "@/types/Student";

--- react-projeto/src/components/StudentTable.tsx
+++ react-projeto/src/components/StudentTable.tsx
@@ -16,7 +16,27 @@ export const StudentTable = ({ students }: Props) => {
           <th className="p-3">Final Grade</th>
         </tr>
       </thead>
+
       <tbody>
+        {students.map(item => (
+          <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+            <td className="p-3 flex items-center">
+              <img src={item.avatar} alt={item.name} className="w-30 h-30 rouded-full mr-5"/>
+              <div>
+                <div className="font-bold">{item.name}</div>
+                <div>{item.email}</div>
+              </div>
+            </td>
+            <td>
+              {item.active && <div>Active</div>}
+              {!item.active && <div>Inactive</div>}
+            </td>
+            <td>{item.grade1}</td>
+            <td>{item.grade2}</td>
+        <td>{item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--'}</td>
+           
+          </tr>
+        ))}
       </tbody>
     </table>
   );
