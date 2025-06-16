aa6519461b63a02246fd6eaa4b38e2a1984989bb
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-16 15:37:36 -0300

    grade / @/types/Student

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -4,6 +4,7 @@ import { students } from "@/data/Students";
 const Page = () => {
   return (
     <div className="container mx-auto">
+      <h1 className="text-5xl mb-5">Lista de estudantes</h1>
       <StudentTable students={students} />
     </div>
   );
--- react-projeto/src/components/StudentTable.tsx
+++ react-projeto/src/components/StudentTable.tsx
@@ -1,4 +1,4 @@
-import {Students} from "@/types/Student"; 
+import { Students } from "@/types/Student";
 
 type Props = {
   students: Students[];
@@ -6,6 +6,18 @@ type Props = {
 
 export const StudentTable = ({ students }: Props) => {
   return (
-    <div>........aaa1</div>
+    <table className="w-full border border-gray-300 rounded-md overflow-hidden">
+      <thead>
+        <tr className="text-left border border-gray-300 bg-gray-800">
+          <th className="p-3"> Name</th>
+          <th className="p-3">Status</th>
+          <th className="p-3">Grade 1</th>
+          <th className="p-3">Grade 2</th>
+          <th className="p-3">Final Grade</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
   );
 };
