f4353fc09c73038d1d0da76022e06606ea4bb753
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-22 17:17:01 -0300

    setShowSecret/ for button

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,13 +1,30 @@
-import { StudentTable } from "@/components/StudentTable";
-import { students } from "@/data/Students";
+"use client"
+import { useState } from 'react';
+import "./globals.css";
 
 const Page = () => {
+  const [showSecret, setShowSecret] = useState(false);
+
+  const handleClickButton = () => {
+    setShowSecret(!showSecret);
+  };
+
   return (
-    <div className="container mx-auto">
-      <h1 className="text-5xl mb-5">Lista de estudantes</h1>
-      <StudentTable students={students} />
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <button
+        onClick={handleClickButton}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors"
+      >
+        Mostrar
+      </button>
+
+      {showSecret && (
+        <div className="p-3 bg-blue-300 rounded-md mt-3">
+          parte secreta
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Page;
