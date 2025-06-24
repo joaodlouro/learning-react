bf8550ad5581bc7e8792bbc9ff1743ec1be112d5
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-24 17:39:20 -0300

    etCount(count + 2);

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,32 +1,27 @@
 "use client"
 
-import { useState } from 'react';
-import React from 'react';
-import "./globals.css";
+ import { useState } from "react";
+ import "./globals.css";
 
-const Page = () => {
-  const [showSecret, setShowSecret] = useState(false);
+ const Page = () => {
+  const [count, setCount] = useState(0);
 
-  const handleClickButton = () => {
-    setShowSecret(!showSecret);
+  const handleBtnClick = () => {
+    setCount(count + 2);
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
-      <button
-        onClick={handleClickButton}
-        className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
-      >
-        Mostrar/Ocultar
-      </button>
 
-      {showSecret &&
-        <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4">
-          aquiiii
-        </div>
-      }
-    </div>
-  );
-}
+  return (
+  <div className="w-screen h-screen flex flex-col items-center justify-center text-3xl">
+    <p>{count}</p>
+
+    <button
+      onClick={handleBtnClick}
+      className="bg-blue-500 text-white px-10 py-2 rounded-md hover:bg-blue-600 transition-colors mt-4"
+    >
+      Click me
+    </button>
+  </div>
+)}
 
 export default Page;
