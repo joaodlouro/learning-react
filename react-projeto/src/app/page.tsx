f1db73e2c372461e402ba9b3d0bb17600f46b012
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-24 21:48:59 -0300

    Perdrinho123 lima

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,29 +1,43 @@
-"use client"
+"use client";
+import { useState } from "react";
+import { Person } from "@/types/Person";
+import "./globals.css";
+//doideira Perdrinho123 lima lol
+const Page = () => {
+  const [fullName, setFullName] = useState<Person>({
+    name: "Boni",
+    lastName: "Lima",
+  });
 
- import { useState } from "react";
- import "./globals.css";
-
- const Page = () => {
-  const [count, setCount] = useState(0);
-
-  const handleBtnClick = () => {
-   setCount( c => c + 2);
-    setCount( c => c + 2);
-     setCount(c => c + 2);
+  const handleClearButton = () => {
+    setFullName({ ...fullName, name: "pedrinho123" });
   };
 
-
   return (
-  <div className="w-screen h-screen flex flex-col items-center justify-center text-3xl">
-    <p>{count}</p>
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <input
+        type="text"
+        placeholder="Sobrenome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3"
+        value={fullName.lastName}
+        onChange={(e) =>
+          setFullName({ ...fullName, lastName: e.target.value })
+        }
+      />
 
-    <button
-      onClick={handleBtnClick}
-      className="bg-blue-500 text-white px-10 py-2 rounded-md hover:bg-blue-600 transition-colors mt-4"
-    >
-      Click me
-    </button>
-  </div>
-)}
+      <p>Nome aqui:</p>
+      <p>
+        {fullName.name} {fullName.lastName}
+      </p>
+
+      <button
+        onClick={handleClearButton}
+        className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+      > Limpar nome aqui </button>
+
+
+    </div>
+  );
+};
 
 export default Page;
--- react-projeto/src/types/Person.ts
+++ react-projeto/src/types/Person.ts
@@ -1,7 +1,4 @@
-export type Person = {
-
-id:number;
-name: string;
-profession: string;
-
-}
+export type Person= {
+  name: string;
+  lastName: string;
+};
