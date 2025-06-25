e7d6f5bdb05c35efcc315f9e3ab8108e744d3d82
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-25 11:54:37 -0300

    limpar nome / mudança de string apenas

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -41,7 +41,7 @@ const Page = () => {
       <button
         onClick={handleClearButton}
         className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-      > Limpar nome aqui </button>
+      > Limpar  nome</button>
 
 
     </div>
