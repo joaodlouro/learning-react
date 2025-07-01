5930fc667b87e507e8d87cb208826738ff8eb1a8
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-30 23:53:02 -0300

    section  grid

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { photosList } from "@/data/PhotoList";
 import "./globals.css";
 
 const Page = () => {
   return (
-    <div className="">
+    <div className="text-center mx-5 font-bold my-10">
       <h1>Fotos interativas</h1>
 
-      <section className="container max-w-5xl mx-auto">
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-35">
+        {photosList.map((item   => (
+          <div> aaaa</div>
+        )))}
         </section>
     </div>
   );
--- react-projeto/src/data/PhotoList.ts
+++ react-projeto/src/data/PhotoList.ts
@@ -1,5 +1,12 @@
 import { Photo } from '../types/Photo';
 
 export const photosList: Photo[] = [
+  {id: 1, url: 'cat-1.jpg'},
+  {id: 2, url: 'cat-2.jpg'},
+  {id: 3, url: 'cat-3.jpg'},
+  {id: 4, url: 'cat-4.jpg'},
+  {id: 5, url: 'cat-5.jpg'},
+  {id: 6, url: 'cat-6.jpg'},
 
 ];
+//lista de fotos
