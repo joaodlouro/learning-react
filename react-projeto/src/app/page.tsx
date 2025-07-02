5ac2c598f80e8d01d4b9b8071c2064500868abd4
joaovx1315 <joaovitorvx1315@gmail.com>
2025-07-01 22:30:26 -0300

    modal fidex and progrees

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -6,11 +6,9 @@ import { PhotoItem } from "@/components/PhotoItem";
 import { useState } from "react";
 import { Modal } from "@/components/Modal";
 
-
 const Page = () => {
-
   const [showModal, setShowModal] = useState(false);
-  const[imageModal, setImageModal] = useState("");
+  const [imageModal, setImageModal] = useState("");
 
   const openModal = (image: number) => {
     const photo = photosList.find((item) => item.id === image);
@@ -18,31 +16,28 @@ const Page = () => {
       setImageModal(photo.url);
       setShowModal(true);
     }
+  };
 
-    const closeModal = () => {
-      setShowModal(false);
-      setImageModal("");
-    }
-
-}
+  const closeModal = () => {
+    setShowModal(false);
+    setImageModal("");
+  };
 
   return (
     <div className="text-center mx-5 font-bold my-10">
       <h1>Fotos interativas</h1>
 
       <section className="container max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-35">
-        {photosList.map((item   => (  
+        {photosList.map((item) => (
           <PhotoItem
             key={item.id}
             photo={item}
-            onClick={()=> { openModal(item.id);}}
-            />
-        )))}
-        </section>
-       {showModal && (
-        <Modal image={imageModal} ocloseModal={closeModal} />
-      }
-        
+            onClick={() => openModal(item.id)}
+          />
+        ))}
+      </section>
+
+      {showModal && <Modal image={imageModal} closeModal={closeModal} />}
     </div>
   );
 };
--- react-projeto/src/components/Modal.tsx
+++ react-projeto/src/components/Modal.tsx
@@ -1,5 +1,18 @@
-export const Modal = () => {
-  return(
-    <div> aaaaa</div>
-  )
+type Props = {
+  image: string;  
+  closeModal: () => void;
+};
+
+export const Modal = ({ image, closeModal }: Props) => {
+  return (
+    <>
+      <div className="fixed left-0 bottom-0 top-0 right-0 flex justify-center items-center bg-black/90">
+        <img src={`/assets/${image}`} alt="" className= ""/>
+      </div>
+
+      <div onClick={closeModal}>
+        x
+      </div>
+    </>
+  );
 };
