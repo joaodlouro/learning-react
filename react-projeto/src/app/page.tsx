d445338482cc37a06d14956acad98cae23f9d1f6
joaovx1315 <joaovitorvx1315@gmail.com>
2025-07-04 00:55:34 -0300

    Quiz 02

--- react-projeto/src/app/page.tsx
+++ react-projeto/src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client"
 
-import React from 'react';
-import "./global.css";
+import { questions } from "@/data/questions";
+import { useState } from "react";
+
+
 
 const Page = () => {
+
+
+const [currentQuestion, setCurrentQuestion] = useState(0);
+  const title = 'Quiz';
 
   return (
 
 
-    <div className="">
+<div className="w-full h-screen flex justify-center bg-blue-600  items-center">
+  <div className="w-full max-w-xl rounded-md bg-white text-black  shadow-black">
+   <div className="p-5 font-bold shadow border-b border-gray-400">{title}</div>
+   <div className="p-5">
+    aaaa
+    </div>
+    <div className="p-5 text-center border-t border-gray-400">
+      {currentQuestion + 1} de pergunts {questions.length} pergunta {questions.length !== 1 ? 's' : ''}
+      </div>
+    
+
+  </div>
 
      
     </div>
