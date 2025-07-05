ba4971b6f66cc72a057a1f87aa15c733039cf09f
joaovx1315 <joaovitorvx1315@gmail.com>
2025-07-04 23:02:39 -0300

    Question  component

---
+++ react-projeto/src/components/QuestionItem.tsx
@@ -0,0 +1,32 @@
+import {Question} from '../types/Question';
+
+
+type Props = {
+
+question: Question;
+count: number;
+onAnswer: (answer: number) => void;
+
+
+
+}
+
+
+export const QuestionItem = ({question,count,onAnswer}: Props) => {
+
+
+
+  return(
+  <div>
+    <div className="">
+
+    </div>
+
+  </div>
+
+  )
+
+
+
+
+}
